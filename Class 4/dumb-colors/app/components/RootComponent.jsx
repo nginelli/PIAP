cd872695ec19b9b6ca24ff08410372b4af9ac34c
Nicole Ginelli <nicoleginelli@gmail.com>
2020-02-26 12:48:13 -0500

    updates

---
+++ Class 4/dumb-colors/app/components/RootComponent.jsx
@@ -0,0 +1,42 @@
+const React = require("react");
+const ClockFace = require("./ClockFace");
+const TwitterList = require("./TwitterList");
+const TwitterForm = require("./TwitterForm");
+
+let dummyData = [];
+
+/* the main page for the index route of this app */
+const RootComponent = function() {
+
+	const [tweets, setTweets] = React.useState([]);
+
+
+	const doFetchTweets = async () => {
+		//fetch the data 
+		const response = await fetch("/api/tweets");
+		const data = await response.json();
+		setTweets(data);
+	};
+
+	//useEffect lets you run a function whenever the state changes
+	// [] listening for the state (or not) 
+	React.useEffect(() => {
+		doFetchTweets();
+	}, []);
+
+ 	 return (
+    <div>
+      <h1>Hello!</h1>
+
+      <p>Your app here</p>
+
+      <ClockFace language="fr" />
+
+      <TwitterForm />
+      <TwitterList tweets={tweets}/>
+
+    </div>
+  );
+}
+
+module.exports = RootComponent;
---
+++ Class 4/dumb-colors/server.js
@@ -0,0 +1,84 @@
+// server.js
+
+// init project
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongouri = process.env.MONGODB_URI || "mongodb://localhost:27017/dumbnicole";
+const mongo = require("mongodb").MongoClient; 
+
+let dbclient;
+
+const dummyData = [
+  {user: "Alex", message: "1"}
+  {user: "Alexa", message: "yo"}
+  {user: "Alex", message: "2"}
+];
+
+
+// Special piece for running with webpack dev server
+if (process.env.NODE_ENV === "development") {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const config = require('./webpack.dev.config.js');
+  const compiler = webpack(config);
+
+  // Tell express to use the webpack-dev-middleware and use the webpack.config.js
+  // configuration file as a base.
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  }));
+}
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/app/index.html');
+});
+
+
+app.get("/api/tweets", async (req, res) => {
+  //json sends clear data that is specific to json 
+  res.json(dummyData);
+
+});
+
+app.get("/counter", async (req, res) => {
+  // Get the collection
+  const collection = await dbclient.collection("siteinfo");
+
+  // Find the document you care about 
+  const item = await collection.findOne( {} );
+
+  //Get your data out of the item 
+  const pageviews = item ? item.pageviews + 1: 1; 
+
+  //Save back to the database 
+  await collection.updateOne({}, {
+    $set: {
+      pageviews: pageviews
+    }
+  }, {
+    upsert: true
+  });
+
+
+  res.send(`This page has been visited ${pageviews} times`);
+});
+
+// listen for requests :)
+mongo.connect(mongouri, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}).then((client) => {
+  
+  dbclient = client.db();
+
+  const listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+  });
+
+})
+
