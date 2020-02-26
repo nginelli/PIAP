dbf4ab44588b6154891e795f5b3823d460607f64
Nicole Ginelli <nicoleginelli@gmail.com>
2020-02-26 17:26:55 -0500

    updates

---
+++ Homework/C2-Click Adventure/chance.js
@@ -0,0 +1,10 @@
+const chance = require("chance"); // load the chance module
+const c = new chance(); // Create an actual chance instace. See the docs.
+
+// console.log(`Your new random name is ${c.name()}`);
+// console.log(`You live on ${c.street()} in ${c.state()}`);
+
+module.exports = {
+    randomName: () => `c.name()`,
+    randomPlace: () => `${c.street()}, ${c.state()}`
+}
