3123e0b820933a7ad35fc92484316c1d4c093d10
Nicole Ginelli <nicoleginelli@gmail.com>
2020-02-27 00:52:02 -0500

    updates

--- Homework/C2-Click Adventure/server.js
+++ Homework/C2-Click Adventure/server.js
@@ -11,59 +11,139 @@ app.use(express.static("public"));
 app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
-let counter = 0;
-// let name = null;
+const locations = [
+    {
+        name: "hallway",
+        template: "hallway.html"
+    }
+];
 
-app.get("/", (req, res) => {
-    counter++;
+// Game state
+let inventory = [];
+let lastLocation = undefined;
+
+function makeContent(title, text, options) {
     let htmldoc = fs.readFileSync("./templates/index.html", "utf8");
-    htmldoc = htmldoc.replace("%%%VIEWS%%%", counter);
+    htmldoc = htmldoc.replace("%%%TITLE%%%", title);
+    htmldoc = htmldoc.replace("%%%TEXT%%%", text);
+    htmldoc = htmldoc.replace("%%%OPTIONS%%%", options.map(option => `<li>${option}</li>`).join(""));
+    return htmldoc;
+}
+
+app.get("/", (_, res) => {
+    inventory = [];
+    lastLocation = "hallway";
+    res.redirect("/hallway");
+});
+
+app.get("/hallway", (_, res) => {
+    lastLocation = "hallway";
+    options = [
+        "<a href=/big>Go left to the big room</a>",
+        "<a href=/small>Go right to the small room</a>"
+    ];
+    options.push("<a href=/inventory>Check your inventory</a>");
     res.send(
-        htmldoc
+        makeContent(
+            "Hallway",
+            "You're standing in the hallway. To your left is a big room. To your right is a small room.",
+            options
+        )
     );
 });
 
-// You can use replace to pretend like you're using React
-app.get("/door", (req, res) => {
-    const newName = randomName();
-    const newAddress = randomPlace();
-    let htmldoc = fs.readFileSync("./templates/door.html", "utf8");
-    htmldoc = htmldoc.replace("%%%NAME%%%", newName);
-    htmldoc = htmldoc.replace("%%%PLACE%%%", newAddress);
+app.get("/big", (_, res) => {
+    lastLocation = "big";
+    let options = ["<a href=/hallway>Go back to the hallway</a>"];
+    options.push("<a href=/unlock>Open the door</a>");
+    options.push("<a href=/inventory>Check your inventory</a>");
     res.send(
-        htmldoc
+        makeContent(
+            "Big room",
+            "You're standing in a big room. It's so big! Echo! There's a heavy looking door in here.",
+            options
+        )
     );
 });
 
-app.get("/window", (req, res) => {
-    const currentName = newName;
-    let htmldoc = fs.readFileSync("./templates/window.html", "utf8");
-    htmldoc = htmldoc.replace("%%%CURRENT_NAME%%%", currentName);
+app.get("/small", (_, res) => {
+    lastLocation = "small";
+    let options = ["<a href=/hallway>Go back to the hallway</a>"];
+    let text = "You're standing in a small room. Actually, it's so small you're not standing, you're crouching.";
+    if (inventory.length === 0) {
+        text += " There's a shiny key on the floor.";
+        options.push("<a href=/key>Pick up the key</a>");
+    }
+    options.push("<a href=/inventory>Check your inventory</a>");
     res.send(
-        htmldoc
+        makeContent(
+            "Small room",
+            text,
+            options
+        )
     );
 });
 
-app.get("/set-name", (req, res) => {
-    let htmldoc = fs.readFileSync("./templates/set-name.html", "utf8");
+app.get("/inventory", (_, res) => {
+    let text = "There is nothing in your inventory!";
+    if (inventory.length > 0) {
+        text = "You have a key in your inventory";
+    }
+    let returnAddress = "hallway";
+    if (!!lastLocation) returnAddress = lastLocation;
     res.send(
-        htmldoc
-    );
+        makeContent(
+            "Inventory",
+            text,
+            [
+                `<a href=/${returnAddress}>Back whence you came</a>`,
+            ]
+        )
+    )
 });
 
-app.get("/inventory", (req, res) => {
-    counter++;
-    let htmldoc = fs.readFileSync("./templates/inventory.html", "utf8");
-    htmldoc = htmldoc.replace("%%%VIEWS%%%", counter);
+app.get("/key", (_, res) => {
+    let text = "You picked up the key!";
+    if (inventory.length === 0) {
+        inventory.push("key");
+    } else {
+        text = "You already have the key";
+    }
+    let returnAddress = "hallway";
+    if (!!lastLocation) returnAddress = lastLocation;
     res.send(
-        htmldoc
-    );
+        makeContent(
+            "Got item",
+            text,
+            [
+                `<a href=/${returnAddress}>Back whence you came</a>`,
+            ]
+        )
+    )
 });
 
-app.post("/set-name", (req, res) => {
-    const suggestion = req.body.username;
+app.get("/unlock", (_, res) => {
+    let text;
+    let returnAddress = "hallway";
+    let options;
+    if (!!lastLocation) returnAddress = lastLocation;
+    
+    if (inventory.length > 0) {
+        text = "You did it! The heavy door latches open with a loud clunk, and swings open dramatically. You've escaped the labyrinth.";
+        options = [`<a href=/>Start over</a>`];
+    } else {
+        text = "You can't unlock the door without a key";
+        options = [`<a href=/${returnAddress}>Back whence you came</a>`];
+    }
+    res.send(
+        makeContent(
+            inventory.length > 0 ? "Victory!" : "Nice try",
+            text,
+            options
+        )
+    )
 });
 
 app.listen(port, () => {
-    console.log(`Express is listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
