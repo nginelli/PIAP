5b56b64ae2f39eedb001cfd5fb11269ff5b6b8e1
Nicole Ginelli <nicoleginelli@gmail.com>
2020-04-01 18:26:19 -0400

    Update app.js

--- Homework/C8-Visualizer/app/app.js
+++ Homework/C8-Visualizer/app/app.js
@@ -3,6 +3,9 @@ const p5 = require("p5");
 const dat = require("dat.gui");
 
 let lastFeatures; 
+let theShader;
+let cam;
+
 
 function prepareMicrophone(callback) {
     navigator.mediaDevices.getUserMedia({audio:true, video:false}).then((stream) => {
@@ -16,9 +19,9 @@ function prepareMicrophone(callback) {
 }
 
 prepareMicrophone((context, source) => {
-    let newBlock = document.createElement("h1");
-    newBlock.innerHTML = "Microphone on!";
-    document.body.appendChild(newBlock);   
+    // let newBlock = document.createElement("h1");
+    // newBlock.innerHTML = "Microphone on!";
+    // document.body.appendChild(newBlock);   
 
     const analyzer = Meyda.createMeydaAnalyzer({
         audioContext: context, 
@@ -34,66 +37,58 @@ prepareMicrophone((context, source) => {
 });
 
 // p5
-const basicDrawing = (p) => {
+const GLDrawing = (p) => {
 
-
-    p.setup = () => {
-        p.createCanvas(700, 400);
-        p.background(10, 255, 10);
-    };
-    p.draw = () => {
-        p.colorMode(p.RGB, 255);
-        p.background(255, 255, 255);
-        if (lastFeatures){
-        
-        p.noFill();
-        // specific will be an array of 24 values 
-        lastFeatures.loudness.specific.forEach((loudness, i) =>{
-            const radius = loudness * 100;
-            p.colorMode(p.HSB, 255);
-            p.strokeWeight(6);
-            const hue = 255 * i / lastFeatures.loudness.specific.length;
-            p.stroke(hue, 255, 255, 100);
-            p.ellipse(p.width / 2, p.height / 2, radius, radius);
-        }); 
-
-        }
-    }
-};
-
-const chromaDrawing = (p) => {
         //object that dat.gui refers to 
         const params = {
-            scale: 200
+            scale: 90,
+            background: 0,
         };
         const gui = new dat.GUI();
-        gui.add(params, "scale", 0, 400);
+        gui.add(params, "scale", 0, 500);
+        gui.add(params, "background", 0, 255);
+
+    p.preload = () => {
+            // load the shader
+            theShader = loadShader('assets/webcam.vert', 'assets/webcam.frag');
+    };
 
     p.setup = () => {
-        p.createCanvas(700, 400);
+        p.createCanvas(p.displayWidth, p.displayHeight, p.WEBGL);
         p.background(10, 255, 10);
-    };
-    p.draw = () => {
-        p.colorMode(p.RGB, 255);
-        p.background(255, 255, 255);
-        if (lastFeatures){
-            
-            lastFeatures.chroma.forEach((c, i) => {
-                const angle = Math.PI * 2 * i / lastFeatures.chroma.length;
-                const angleWidth = (2 * Math.PI) / lastFeatures.chroma.length;
-                const radius = c * params.scale;
-                // const radius = c * 200;
-                const hue = 255 * i / lastFeatures.chroma.length;
-                // const hue = 255 * c / lastFeatures.chroma.length;
-                p.colorMode(p.HSB, 255);
-                p.fill(hue, 255, 255);
-                p.arc(p.width / 2, p.height / 2, radius, radius, angle, angle + angleWidth);
-            });
+        p.directionalLight(125, 125, 125, 0, 5, 8);
 
+        cam = p.createCapture(p.VIDEO);
+        cam.size(710, 400);
+     
+        cam.hide();
+    };
+    
+    p.draw = () => {
+        p.colorMode(p.HSB, 255);
+        p.background(params.background, 255, params.background);
+        shader(theShader);
+              
+        if (lastFeatures){
+
+        lastFeatures.loudness.specific.forEach((loudness, i) =>{
+
+            const radius = loudness * params.scale;
+
+            p.rotateX(p.frameCount * 0.01);
+            p.rotateY(p.frameCount * 0.01);
+            // p.rotateY(p.mouseY * 0.01);
+
+            p.sphere(radius, 5, 100);
+            // p.normalMaterial();     
+
+        });
+
+
+        
 
         }
     }
 };
 
-// const myp5 = new p5(basicDrawing, "main");
-const myp5 = new p5(chromaDrawing, "main");
+const myp5 = new p5(GLDrawing, "main");
