01ee42c54f6650ed3dd416a4d90d0010b51f98f0
Nicole Ginelli <nicoleginelli@gmail.com>
2020-04-08 22:01:38 -0400

    update

--- Class 10-Language/poem/poem.js
+++ Class 10-Language/poem/poem.js
@@ -1,11 +1,192 @@
-async function makePoem() {
+const osmosis = require("osmosis");
+const sbd = require("sbd");
+const syllable = require("syllable");
+const pos = require("pos");
+const lexer = new pos.Lexer();
+const tagger = new pos.Tagger();
+const rhyme = require("rhyme");
+
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+function scramble(array) {
+    for (let i = 0; i < array.length; i++) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        const tmp = array[randomIndex];
+        array[randomIndex] = array[i];
+        array[i] = tmp;
+    }
+}
+
+function cleanText(text) {
+    // find any place you have [number] and get rid of it 
+    // regexr.com to test out "regular expressions" lets u define sets of characters
+    return text.replace(/\[[0-9]+\]/g, "");
+}
+
+async function loadRhymingDictionary() {
+    return new Promise((resolve) => {
+    rhyme((rhymingDictionary) => {
+        resolve(rhymingDictionary);
+         });
+    });
+}
+
+
+async function getText(){
+    return new Promise((resolve, reject) => {
+        let text = [];
+        osmosis.get("https://en.wikipedia.org/wiki/Copper")
+            .find("p")
+            //finds and returns all the paragraphs on that page
+            .set("paragraph")
+            .data((item) => text.push(item.paragraph))
+            .done(() => resolve(text))
+            .error((e) => reject(e));
+    });
+}
+async function firstWordPoem() {
+    const paragraphs = await getText();
+    let firstWords = [];
+    paragraphs.forEach(paragraph => {
+        const firstWord = paragraph.split(" ")[0];
+        firstWords.push(firstWord);
+    });
+    return firstWords.join(" ");
+}
+
+async function haikuPoem() {
+    const paragraphs = await getText();
+    let fragments = [];
+    paragraphs.forEach(pg => {
+        let cleanpg = cleanText(pg);
+        const sentences = sbd.sentences(pg);
+        sentences.forEach(sentence => {
+            const chunks = sentence.split(",");
+            fragments = fragments.concat(chunks);
+        });
+    });
+
+    const fiveSyllableFragments = fragments.filter(fragment => {
+        return syllable(fragment) === 5;
+    });
+
+    const sevenSyllableFragments = fragments.filter(fragment => {
+        return syllable(fragment) === 7;
+    });
+
+    scramble(fiveSyllableFragments);
+    scramble(sevenSyllableFragments);
+
+
     return [
-        "This is the first line",
-        "And this, the second"
-    ];
+        fiveSyllableFragments[0],
+        sevenSyllableFragments[0],
+        fiveSyllableFragments[1]
+    ]
+}
+
+async function posPoem() {
+    const paragraph = await getText();
+    const posTypes = ["JJR"];
+    const tokens = [];
+
+    paragraph.forEach(pg => {
+        const cleanpg = cleanText(pg);
+        const sentences = sbd.sentences(cleanpg);
+        sentences.forEach(sentence => {
+            const lexes = lexer.lex(sentence);
+            const tags = tagger.tag(lexes);
+            tags.forEach(tag => {
+                if (posTypes.includes(tag[1])) {
+                    if (!tokens.includes(tag[0])) {
+                        tokens.push(tag[0]);
+                    }
+                }
+            });
+        });
+    });
+    return tokens;
+}
+
+async function rhymePoem() {
+    const paragraph = await getText();
+    const rhymeGroups = {};
+    const rd = await loadRhymingDictionary();
+    paragraph.forEach(pg => {
+        const cleanpg = cleanText(pg);
+        const sentences = sbd.sentences(cleanpg);
+        sentences.forEach(sentence => {
+            const lexes = lexer.lex(sentence);
+            for (let i = 0; i < lexes.length -5; i++) {
+                const fragment = lexes.slice(i, i + 5);
+                const lastWord = fragment.slice(-1)[0];
+                const pronunciations = rd.pronounce(lastWord);
+                if (pronunciations) {
+                    const pronunciation = pronunciations[0];
+                    const rhymeClass = pronunciation.slice(-3).join("-")
+                    if (!rhymeGroups[rhymeClass]) rhymeGroups[rhymeClass] =[];
+                    rhymeGroups[rhymeClass].push(fragment);
+                }
+            }
+        });
+    
+    });
+
+    let goodKeys = Object.keys(rhymeGroups);
+    goodKeys = goodKeys.filter(key => {
+        const fragments = rhymeGroups[key];
+        return !fragments.every(fragment => {
+            return fragment.slice(-1)[0].toLowerCase() === fragments[0].slice(-1)[0].toLowerCase();
+        });
+    });
+    
+    function getRhymingPair() {
+        scramble(goodKeys);
+        const rhymeClass = goodKeys[0];
+        const rhymingFragments = rhymeGroups[rhymeClass];
+        scramble(rhymingFragments);
+        const frag1 = rhymingFragments[0];
+        const lastWord = frag1.slice(-1)[0];
+        const otherValidFragments = rhymingFragments.filter(otherFragment => {
+            
+            const otherLastWord = otherFragment.slice(-1)[0];
+            return otherLastWord !== lastWord;
+
+        });
+        const frag2 = otherValidFragments[0];
+        return [ frag1.join(" "), frag2.join(" ") ];
+    }
+
+    let lines = [];
+    for (let i = 0; i < 3; i++) {
+        const pair1 = getRhymingPair();
+        const pair2 = getRhymingPair();
+        lines.push(pair1[0]);
+        lines.push(pair2[0]);
+        lines.push(pair1[0]);
+        lines.push(pair2[0]);
+    }
+
+    lines = lines.concat(getRhymingPair());
+
+    return lines;
+    return getRhymingPair();
+}
+
+
+async function makePoem() {
+    // return firstWordPoem();
+    // return haikuPoem());
+    //  return posPoem();
+    return rhymePoem();
+
+
 }
 
 if (require.main === module) {
+    // getText().then(text => console.log(text));
     makePoem().then(res => console.log(res));
 }
 
